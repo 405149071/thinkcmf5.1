adbb35fb5c75ff15698c6bdb3f22518be8647289
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-04 12:09:54 +0800

    disc

---
+++ app/shc/model/ShcDiscModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * Created by thinkcmf5_1.
+ * User: wuzz
+ * Date: 2019/8/4
+ * Time: 11:21 AM
+ */
+
+namespace app\shc\model;
+
+use think\Model;
+
+class ShcDiscModel extends Model
+{
+
+}
